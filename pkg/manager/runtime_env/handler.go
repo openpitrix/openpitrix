15675e0a1af37bf711304f54b2264d1350b968db
runzexia <root@junwuhui.cn>
2018-03-21 10:53:56 +0800

    move bll codes to handler

--- pkg/manager/runtime_env/handler.go
+++ pkg/manager/runtime_env/handler.go
@@ -14,6 +14,7 @@ import (
 	"openpitrix.io/openpitrix/pkg/logger"
 	"openpitrix.io/openpitrix/pkg/pb"
 	"openpitrix.io/openpitrix/pkg/utils/sender"
+	"openpitrix.io/openpitrix/pkg/models"
 )
 
 func (p *Server) CreateRuntimeEnv(ctx context.Context, req *pb.CreateRuntimeEnvRequest) (*pb.CreateRuntimeEnvResponse, error) {
@@ -75,3 +76,42 @@ func (p *Server) AttachCredentialToRuntimeEnv(ctx context.Context, req *pb.Attac
 func (p *Server) DetachCredentialFromRuntimeEnv(ctx context.Context, req *pb.DetachCredentialFromRuntimeEnvRequset) (*pb.DetachCredentialFromRuntimeEnvResponse, error) {
 	return nil, status.Errorf(codes.Internal, "DetachCredentialFromRuntimeEnv: %+v", fmt.Errorf("hello world"))
 }
+
+
+func (p *Server) getRuntimeEnvPbWithLabel(runtimeEnvId string) (*pb.RuntimeEnv, error) {
+	runtimeEnv, err := p.getRuntimeEnv(runtimeEnvId)
+	if err != nil {
+		logger.Errorf("failed to get runtime_env [%v] ", err)
+		return nil, fmt.Errorf("failed to get runtime_env [%v] ", err)
+	}
+	pbRuntimeEnv := models.RuntimeEnvToPb(runtimeEnv)
+	runtimeEnvLabels, err := p.getRuntimeEnvLabelsByEnvId(runtimeEnvId)
+	if err != nil {
+		logger.Errorf("failed to get runtime_env_label [%v] ", err)
+		return nil, fmt.Errorf("failed to get runtime_env label [%v] ", err)
+	}
+	pbRuntimeEnv.Labels = models.RuntimeEnvLabelsToPbs(runtimeEnvLabels)
+
+	return pbRuntimeEnv, nil
+}
+
+func (p *Server) createRuntimeEnv(name, description, url, userId string) (runtimeEnvId string, err error) {
+	newRuntimeEnv := models.NewRuntimeEnv(name, description, url, userId)
+	err = p.insertRuntimeEnv(*newRuntimeEnv)
+	if err != nil {
+		logger.Errorf("failed to insert runtime_env [%v] ", err)
+		return "", fmt.Errorf("failed to insert runtime_env [%v] ", err)
+	}
+	return newRuntimeEnv.RuntimeEnvId, err
+}
+
+func (p *Server) createRuntimeEnvLabels(runtimeEnvId, labelString string) error {
+	labelMap, err := LabelStringToMap(labelString)
+	if err != nil {
+		logger.Error(err)
+		return err
+	}
+	err = p.insertRuntimeEnvLabels(runtimeEnvId, labelMap)
+	return err
+}
+
