6a7e61811a938f69a2a34167bfc9d4ad5abd3e53
chai2010 <chaishushan@gmail.com>
2018-04-03 20:22:11 +0800

    fix build

--- pkg/service/pilot/handler.go
+++ pkg/service/pilot/handler.go
@@ -9,9 +9,16 @@ import (
 	"fmt"
 	"math/rand"
 
+	pb_empty "github.com/golang/protobuf/ptypes/empty"
+
 	"openpitrix.io/openpitrix/pkg/logger"
 	pb "openpitrix.io/openpitrix/pkg/pb"
 	pb_frontgate "openpitrix.io/openpitrix/pkg/pb/frontgate"
+)
+
+var (
+	_ pb.PilotServiceServer             = (*Server)(nil)
+	_ pb.PilotServiceForFrontgateServer = (*Server)(nil)
 )
 
 func (p *Server) HandleSubtask(context.Context, *pb.HandleSubtaskRequest) (*pb.HandleSubtaskResponse, error) {
@@ -20,6 +27,10 @@ func (p *Server) HandleSubtask(context.Context, *pb.HandleSubtaskRequest) (*pb.H
 
 func (p *Server) GetSubtaskStatus(context.Context, *pb.GetSubtaskStatusRequest) (*pb.GetSubtaskStatusResponse, error) {
 	panic("todo")
+}
+
+func (p *Server) CloseChannel(context.Context, *pb_empty.Empty) (*pb_empty.Empty, error) {
+	panic("TODO")
 }
 
 func (p *Server) Channel(ch pb.PilotServiceForFrontgate_ChannelServer) error {
@@ -71,6 +82,41 @@ func (p *Server) CloseFrontgateClient(id string) error {
 
 	delete(p.clientMap, id)
 	return lastErr
+}
+
+func (p *Server) GetPilotInfo(context.Context, *pb_empty.Empty) (*pb.PilotInfo, error) {
+	panic("TODO")
+}
+func (p *Server) GetFrontgateInfo(context.Context, *pb_empty.Empty) (*pb.FrontgateInfo, error) {
+	panic("TODO")
+}
+
+func (p *Server) GetDroneInfo(context.Context, *pb_empty.Empty) (*pb.DroneInfo, error) {
+	panic("TODO")
+}
+func (p *Server) GetConfdInfo(context.Context, *pb.GetConfdInfoRequest) (*pb.ConfdInfo, error) {
+	panic("TODO")
+}
+
+func (p *Server) StartConfd(context.Context, *pb.StartConfdRequest) (*pb_empty.Empty, error) {
+	panic("TODO")
+}
+func (p *Server) StopConfd(context.Context, *pb.StopConfdRequest) (*pb_empty.Empty, error) {
+	panic("TODO")
+}
+
+func (p *Server) RegisterMetadata(context.Context, *pb.RegisterMetadataRequest) (*pb_empty.Empty, error) {
+	panic("TODO")
+}
+func (p *Server) DeregisterMetadata(context.Context, *pb.DeregisterMetadataRequest) (*pb_empty.Empty, error) {
+	panic("TODO")
+}
+
+func (p *Server) RegisterCmd(context.Context, *pb.RegisterCmdRequest) (*pb_empty.Empty, error) {
+	panic("TODO")
+}
+func (p *Server) DeregisterCmd(context.Context, *pb.DeregisterCmdRequest) (*pb_empty.Empty, error) {
+	panic("TODO")
 }
 
 func (p *Server) putFrontgateClient(id string, c *pb_frontgate.FrontgateServiceClient) {
