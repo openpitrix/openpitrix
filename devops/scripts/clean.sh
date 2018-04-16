45146ac826f0e8cb37afb1c79bb55e435668e85c
Calvin Yu <calvinyu@yunify.com>
2018-04-16 15:10:36 +0800

    add k8s deploy sample templates

--- devops/scripts/clean.sh
+++ devops/scripts/clean.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-echo "clean..."
+echo "clean all stuff"
 
+echo "start to clean k8s resource"
+# Back to the root of the project
+cd $(dirname $0)
+cd ../..
+
+kubectl delete -f ./devops/kubernetes/openpitrix
+kubectl delete -f ./devops/kubernetes/etcd
+kubectl delete -f ./devops/kubernetes/ctrl
+kubectl delete  -f ./devops/kubernetes/db
+
+
+
+echo "start to clean docker resource"
 docker rmi openpitrix/openpitrix-dev:latest
 docker rmi openpitrix
 
--- devops/scripts/deploy-k8s.sh
+++ devops/scripts/deploy-k8s.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+git clone https://github.com/openpitrix/openpitrix.git /opt/openpitrix
 # Back to the root of the project
 cd $(dirname $0)
 cd ../..
 
 kubectl create secret generic mysql-pass --from-file=./devops/kubernetes/password.txt -n default
 kubectl apply -f ./devops/kubernetes/db
+kubectl apply -f ./devops/kubernetes/etcd
 kubectl apply -f ./devops/kubernetes/openpitrix
