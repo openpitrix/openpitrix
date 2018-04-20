951f9f5c67ffbb8d411767ab357f320931bed12c
chilianyi <chilianyi@126.com>
2018-04-20 13:32:21 +0800

    Fix db schema write error

--- pkg/db/schema/job/V0_2__alter.sql
+++ pkg/db/schema/job/V0_2__alter.sql
@@ -1,6 +1,6 @@
-ALTER TABLE cluster
+ALTER TABLE job
 	CHANGE COLUMN runtime provider VARCHAR(50) NOT NULL;
-ALTER TABLE cluster
+ALTER TABLE job
 	DROP INDEX job_runtime_index;
-ALTER TABLE cluster
+ALTER TABLE job
 	ADD INDEX job_provider_index (provider ASC);
