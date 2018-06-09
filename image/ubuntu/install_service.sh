72f00fdc9c58dd3c99fdb1bb3d186dc3540e4be3
chilianyi <chilianyi@126.com>
2018-06-09 18:47:26 +0800

    Prolong the timeout when job wait task and increase the retry times when install docker

--- image/ubuntu/install_service.sh
+++ image/ubuntu/install_service.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 
 FILE_NAME="$1"
 
 chmod +x opt/openpitrix/sbin/*
-for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19;
+for i in $(seq 1 100)
 do
   opt/openpitrix/sbin/install_docker.sh && break || sleep 3
 done
@@ -15,17 +15,17 @@ echo '{
 mkdir -p /opt/openpitrix/conf
 mkdir -p /opt/openpitrix/log
 mkdir -p /opt/openpitrix/sbin
-mkdir -p /etc/confd/conf.d; mv etc/confd/conf.d/cmd.info.toml /etc/confd/conf.d/
-mkdir -p /etc/confd/templates; mv etc/confd/templates/cmd.info.tmpl /etc/confd/templates/
-mv opt/openpitrix/sbin/exec.sh opt/openpitrix/sbin/start_default.sh opt/openpitrix/sbin/update_fstab.sh /opt/openpitrix/sbin/
-mv opt/openpitrix/conf/default.service /lib/systemd/system/default.service
+mkdir -p /etc/confd/conf.d; cp etc/confd/conf.d/cmd.info.toml /etc/confd/conf.d/
+mkdir -p /etc/confd/templates; cp etc/confd/templates/cmd.info.tmpl /etc/confd/templates/
+cp opt/openpitrix/sbin/exec.sh opt/openpitrix/sbin/start_default.sh opt/openpitrix/sbin/update_fstab.sh /opt/openpitrix/sbin/
+cp opt/openpitrix/conf/default.service /lib/systemd/system/default.service
 
 if [ "$FILE_NAME" == "frontgate.conf" ]
 then
   mkdir -p /opt/openpitrix/etcd
-  mv opt/openpitrix/sbin/start_etcd.sh /opt/openpitrix/sbin/
+  cp opt/openpitrix/sbin/start_etcd.sh /opt/openpitrix/sbin/
 
-  mv opt/openpitrix/conf/etcd.service /lib/systemd/system/etcd.service
+  cp opt/openpitrix/conf/etcd.service /lib/systemd/system/etcd.service
   systemctl enable etcd.service && systemctl start etcd.service
 fi
 
