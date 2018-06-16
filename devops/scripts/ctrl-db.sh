881fc41d0e4f04a167b8814ce766ccbedd2c793b
chilianyi <chilianyi@126.com>
2018-06-16 22:00:25 +0800

    Refactor deploy k8s to support multiple model

--- devops/scripts/ctrl-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Back to the root of the project
-cd $(dirname $0)
-cd ../..
-
-kubectl apply -f ./devops/kubernetes/ctrl
-
--- devops/scripts/deploy-k8s.sh
+++ devops/scripts/deploy-k8s.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 DEFAULT_NAMESPACE=openpitrix-system
-DEFAULT_METADATA=off
+DEFAULT_MODEL=default
 
 NAMESPACE=${DEFAULT_NAMESPACE}
+MODEL=${DEFAULT_MODEL}
 
 while getopts n:v:m:h option
 do
@@ -11,9 +12,9 @@ do
   in
   n) NAMESPACE=${OPTARG};;
   v) VERSION=${OPTARG};;
-  m) METADATA=${OPTARG};;
-  h) echo "usage ./deploy-k8s.sh -n namespace -v version -m on" && exit 1 ;;
-  *) echo "usage ./deploy-k8s.sh -n namespace -v version -m on" && exit 1 ;;
+  m) MODEL=${OPTARG};;
+  h) echo "usage ./deploy-k8s.sh -n namespace -v version -m default/all/dbctrl/metadata" && exit 1 ;;
+  *) echo "usage ./deploy-k8s.sh -n namespace -v version -m default/all/dbctrl/metadata" && exit 1 ;;
   esac
 done
 
@@ -38,20 +39,50 @@ cd ../..
 kubectl create namespace ${NAMESPACE}
 kubectl create secret generic mysql-pass --from-file=./devops/kubernetes/password.txt -n ${NAMESPACE}
 
-for FILE in `ls ./devops/kubernetes/db/`;do
-  sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/db/${FILE} | kubectl apply -f -
-done
 
-for FILE in `ls ./devops/kubernetes/etcd/`;do
-  sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/etcd/${FILE} | kubectl apply -f -
-done
-
-for FILE in `ls ./devops/kubernetes/openpitrix/ | grep "^openpitrix-"`;do
-  sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/openpitrix/${FILE} | kubectl apply -f -
-done
-
-if [ "$METADATA" == "on" ];then
+if [ "${MODEL}" == "dbctrl" ];then
+  for FILE in `ls ./devops/kubernetes/ctrl`;do
+    sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/ctrl/${FILE} | kubectl apply -f -
+  done
+  exit 0
+elif [ "${MODEL}" == "metadata" ];then
   for FILE in `ls ./devops/kubernetes/openpitrix/metadata/`;do
     sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/openpitrix/metadata/${FILE} | kubectl apply -f -
   done
+  exit 0
+elif [ "${MODEL}" == "default" ];then
+  for FILE in `ls ./devops/kubernetes/db/`;do
+    sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/db/${FILE} | kubectl apply -f -
+  done
+
+  for FILE in `ls ./devops/kubernetes/etcd/`;do
+    sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/etcd/${FILE} | kubectl apply -f -
+  done
+
+  for FILE in `ls ./devops/kubernetes/openpitrix/ | grep "^openpitrix-"`;do
+    sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/openpitrix/${FILE} | kubectl apply -f -
+  done
+elif [ "${MODEL}" == "all" ];then
+  for FILE in `ls ./devops/kubernetes/ctrl`;do
+    sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/ctrl/${FILE} | kubectl apply -f -
+  done
+
+  for FILE in `ls ./devops/kubernetes/db/`;do
+    sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/db/${FILE} | kubectl apply -f -
+  done
+
+  for FILE in `ls ./devops/kubernetes/etcd/`;do
+    sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/etcd/${FILE} | kubectl apply -f -
+  done
+
+  for FILE in `ls ./devops/kubernetes/openpitrix/ | grep "^openpitrix-"`;do
+    sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/openpitrix/${FILE} | kubectl apply -f -
+  done
+
+  for FILE in `ls ./devops/kubernetes/openpitrix/metadata/`;do
+    sed -e "s!\${NAMESPACE}!${NAMESPACE}!g" -e "s!\${IMAGE}!${IMAGE}!g" -e "s!\${METADATA_IMAGE}!${METADATA_IMAGE}!g" -e "s!\${FLYWAY_IMAGE}!${FLYWAY_IMAGE}!g" ./devops/kubernetes/openpitrix/metadata/${FILE} | kubectl apply -f -
+  done
+else
+  echo "usage ./deploy-k8s.sh -n namespace -v version -m default/all/dbctrl/metadata" && exit 1
 fi
+
