2551a5ff1f952b432785969dcfa5264938ca37d7
hevienz <hevienzdong@yunify.com>
2018-12-20 14:37:07 +0800

    Add ingress for deploying in k8s
    Signed-off-by: hevienz <hevienzdong@yunify.com>

--- deploy/kubernetes/scripts/generate-certs.sh
+++ deploy/kubernetes/scripts/generate-certs.sh
@@ -9,27 +9,31 @@ NAMESPACE=${DEFAULT_NAMESPACE}
 
 usage() {
   echo "Usage:"
-  echo "  generate-certs.sh [-n NAMESPACE]"
+  echo "  generate-certs.sh [-n NAMESPACE] [-c HOST]"
   echo "Description:"
-  echo "    -n NAMESPACE: the namespace of kubernetes."
+  echo "    -n NAMESPACE : the namespace of kubernetes."
+  echo "    -c HOST      : the hostname used in certificate."
   exit -1
 }
 
-while getopts n: option
+while getopts n:c: option
 do
   case "${option}"
   in
     n) NAMESPACE=${OPTARG};;
+    c) HOST=${OPTARG};;
   esac
 done
 
 cd $(dirname $0)
 cd ../..
 cd ./kubernetes/tls-config
-make
+make ingress.crt-${HOST}
 
 SECRETS=("openpitrix-ca.crt" "pilot-server.crt" "pilot-server.key" "pilot-client.crt" "pilot-client.key")
 for i in ${SECRETS[@]}; do
   echo ${i}
   kubectl create secret generic ${i} --from-file=./${i} -n ${NAMESPACE} || true
 done
+
+kubectl create secret tls ingress-tls --key ingress.key --cert ingress.crt -n ${NAMESPACE} || true
