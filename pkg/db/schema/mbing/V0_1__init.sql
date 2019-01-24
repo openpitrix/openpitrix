4834613a9a845fff93477926282ec1edb9c42420
yudong2015 <2091381910@qq.ccom>
2019-01-24 19:55:55 +0800

    modify tables of mbing

--- pkg/db/schema/mbing/V0_1__init.sql
+++ pkg/db/schema/mbing/V0_1__init.sql
@@ -1,87 +1,153 @@
 /**  Price  **/
 CREATE TABLE IF NOT EXISTS attribute
 (
-	id 										INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE ,
-	name 									VARCHAR(50) NOT NULL,
-	display_name 					VARCHAR(50),
-	create_time 					TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	id 										VARCHAR(50) 		NOT NULL UNIQUE ,
+	name 									VARCHAR(255) 		NOT NULL,
+	display_name 					VARCHAR(255),
+	create_time 					TIMESTAMP 			NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_time 					TIMESTAMP,
+	status								TINYINT 				DEFAULT 1 COMMENT '1: using, 0: deleted',
 	mark 									TEXT,
 	PRIMARY KEY (id)
-	id 										INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE ,
 );
 
 CREATE TABLE IF NOT EXISTS attribute_unit
 (
-	id 										INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE ,
-	name 									VARCHAR(255) NOT NULL,
+	id 										VARCHAR(50) 		NOT NULL UNIQUE ,
+	name 									VARCHAR(30)		 	NOT NULL,
+	create_time 					TIMESTAMP 			NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_time 					TIMESTAMP,
+	status								TINYINT 				DEFAULT 1 COMMENT '1: using, 0: deleted',
 )
 
 CREATE TABLE IF NOT EXISTS attribute_value
 (
-	id 										INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE ,
-	attribute_id 					INT NOT NULL,
-	attribute_unit_id 		INT NOT NULL,
-	min_value 						INT NOT NULL ,
-	upto_value						INT NOT NULL COMMENT 'the attribute value, support scope of value [min_value, max_value);',
-	create_time 					TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	id 										VARCHAR(50) 		NOT NULL UNIQUE ,
+	attribute_id 					VARCHAR(50) 		NOT NULL,
+	attribute_unit_id 		VARCHAR(50) 		NOT NULL,
+	min_value 						INT 						NOT NULL ,
+	max_value					  	INT 						NOT NULL COMMENT 'the attribute value, support scope of value [min_value, max_value);',
+	create_time 					TIMESTAMP 			NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_time 					TIMESTAMP,
+	status								TINYINT 				DEFAULT 1 COMMENT '1: using, 0: deleted',
 	PRIMARY KEY (id)
 );
 
 
 CREATE TABLE IF NOT EXISTS resource_attribute
 (
-	id 										INT UNSIGNED 			NOT NULL AUTO_INCREMENT UNIQUE ,
-	resource_version_id 	VARCHAR(50)		NOT NULL,
-	attributes 						JSON NOT NULL COMMENT 'sku attribute ids',
-	metering_attributes 	JSON NOT NULL COMMENT 'the attribute ids need to metering',
-	billing_attributes	 	JSON NOT NULL COMMENT 'the attribute ids for billing',
-	create_time 					TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	id 										VARCHAR(50) 		NOT NULL UNIQUE ,
+	resource_version_id 	VARCHAR(50)			NOT NULL,
+	attributes 						JSON 						NOT NULL COMMENT 'sku attribute ids',
+	metering_attributes 	JSON 						NOT NULL COMMENT 'the attribute ids need to metering',
+	billing_attributes	 	JSON 						NOT NULL COMMENT 'the attribute ids for billing',
+	create_time 					TIMESTAMP 			NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_time 					TIMESTAMP,
+	status								TINYINT 				DEFAULT 1 COMMENT '1: using, 0: deleted',
 	PRIMARY KEY (id)
 );
 
 
-CREATE TABLE IF NOT EXISTS SKU
+CREATE TABLE IF NOT EXISTS sku
 (
-	id INT UNSIGNED 			NOT NULL AUTO_INCREMENT UNIQUE ,
+	id 											VARCHAR(50) 	NOT NULL UNIQUE ,
 	resource_attribute_id 	VARCHAR(50)		NOT NULL,
-	values 								JSON NOT NULL COMMENT 'sku attribute_value for attribute in resource_attribute',
-	metering_attributes 	JSON NOT NULL COMMENT 'the attribute ids need to metering',
-	billing_attributes	 	JSON NOT NULL COMMENT 'the attribute ids for billing',
-	create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	values 									JSON 					NOT NULL COMMENT 'sku attribute values for attributes in resource_attribute: {attribute: value, ...}',
+	create_time 						TIMESTAMP 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_time 						TIMESTAMP,
+	status									TINYINT 			DEFAULT 1 COMMENT '1: using, 0: deleted',
 	PRIMARY KEY (id)
 );
-
 
 
 CREATE TABLE IF NOT EXISTS price
 (
-	id 									VARCHAR(50)		NOT NULL,
-	resource_version_id VARCHAR(50) 	NOT NULL,
-	charge_mode 				TINYINT			NOT NULL COMMENT 'ELASTIC:0, MONTHLY:1, YEARLY:2',
-	price 							INT(11)		 	NOT NULL COMMENT 'measure: cent; currency * 100',
-	currency            VARCHAR(50),
-	duration 						INT(11),
-	count 							INT(11),
-	free_time 					INT(11) COMMENT 'measure: hour',
-	create_time       	TIMESTAMP	    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	rule 								TEXT,
-	INDEX price_resource_index (resource_version_id, charge_mode),
+	id 											VARCHAR(50) 	NOT NULL UNIQUE ,
+	sku_id 									VARCHAR(50) 	NOT NULL,
+	billing_attribute_id	 	VARCHAR(50)		NOT NULL ,
+	prices 									JSON 					commment '{attribute_value1: price1, ...}',
+	currency            		VARCHAR(50)		NOT NULL  DEFAULT 'cny',
+	create_time       			TIMESTAMP	    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_time       			TIMESTAMP,
+	status									TINYINT 			DEFAULT 1 COMMENT '1: using, 0: deleted',
+	INDEX price_sku_index (sku_id, id),
 	PRIMARY KEY (id)
 );
 
+
+CREATE TABLE IF NOT EXISTS combination_resource_attribute
+(
+	id 										VARCHAR(50) NOT NULL UNIQUE ,
+	resource_version_ids 	JSON NOT NULL COMMENT 'combination resource version id',
+	attributes 						JSON NOT NULL COMMENT 'sku attribute ids: {resource_version_id:[],...}',
+	metering_attributes 	JSON NOT NULL COMMENT 'the attribute ids need to metering: {resource_version_id:{}, ..}',
+	billing_attributes	 	JSON NOT NULL COMMENT 'the attribute ids for billing: {resource_version_id:{}, ..}',
+	create_time 					TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_time 					TIMESTAMP,
+	status								TINYINT DEFAULT 1 COMMENT '1: using, 0: deleted',
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS combination_sku
+(
+	id 											VARCHAR(50) NOT NULL UNIQUE ,
+	resource_attribute_id 	VARCHAR(50)		NOT NULL,
+	values 									JSON NOT NULL COMMENT 'sku attribute values for attributes in resource_attribute: {resource_version_id:{}, ..}',
+	create_time 						TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_time 						TIMESTAMP,
+	status									TINYINT DEFAULT 1 COMMENT '1: using, 0: deleted',
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS combination_price
+(
+	id 											VARCHAR(50) 	NOT NULL UNIQUE ,
+	combination_sku_id 			VARCHAR(50) 	NOT NULL,
+	billing_attribute_id	 	JSON					COMMENT '{resource_version_id: attribute_id}' ,
+	prices 									JSON 					COMMENT '{attribute_value1: price1, ...}',
+	currency            		VARCHAR(50)		NOT NULL  DEFAULT 'cny',
+	create_time       			TIMESTAMP	    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_time       			TIMESTAMP,
+	status									TINYINT 			DEFAULT 1 COMMENT '1: using, 0: deleted',
+	INDEX price_sku_index (combination_sku_id, id),
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS probation_sku
+(
+	id 											VARCHAR(50) NOT NULL UNIQUE ,
+	resource_attribute_id 	VARCHAR(50)		NOT NULL,
+	values 									JSON NOT NULL COMMENT 'sku attribute values for attributes in resource_attribute: {attribute: value, ...}',
+	create_time 						TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	update_time 						TIMESTAMP,
+	status									TINYINT DEFAULT 1 COMMENT '1: using, 0: deleted',
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS probation_record
+(
+	probation_sku_id 				VARCHAR(50) 	NOT NULL,
+	user_id 								VARCHAR(50) 	NOT NULL,
+	create_time 						TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (probation_id, user_id)
+)
+
+
 CREATE TABLE IF NOT EXISTS dicount
 (
-	id 							VARCHAR(50) 	NOT NULL,
-	user_id					VARCHAR(50),
-	name 						VARCHAR(255)	NOT NULL,
-	price_id				VARCHAR(50) 	NOT NULL,
-	new_price 			DECIMAL(8, 2),
-	discount 				FLOAT(3, 2),
-	dicount_type 		TINYINT		 	NOT NULL,
-	start_time		  TIMESTAMP 	    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	end_time       	TIMESTAMP   	NOT NULL,
-	create_time     TIMESTAMP	    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	mark 						TEXT,
+	id 									VARCHAR(50) 	NOT NULL,
+	name 								VARCHAR(255)	NOT NULL,
+	limit								JSON 					COMMENT '{resource:.., sku:.., price:.., user:..,}',
+	discount_value 			DECIMAL(8, 2) COMMENT 'the price value to cut down',
+	discount_percent 		DECIMAL(1, 2) COMMENT 'the price percent to cut down, there is only one of discount_value and discount_percent',
+	start_time		  		TIMESTAMP 	  NOT NULL,
+	end_time       			TIMESTAMP   	NOT NULL,
+	create_time     		TIMESTAMP	    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	mark 								TEXT,
 	PRIMARY KEY (id)
 );
 
@@ -90,19 +156,31 @@ CREATE TABLE IF NOT EXISTS coupon
 (
 	id 									VARCHAR(50) 	NOT NULL,
 	name 								VARCHAR(50) 	NOT NULL,
-	sn 									VARCHAR(50),
-	quota 							DECIMAL(8, 2) 	NOT NULL,
-	balance 						DECIMAL(8, 2) 	NOT NULL,
-	coupon_type 				TINYINT			NOT NULL COMMENT 'MONEY:0, TIME:1',
-	resource_version_id VARCHAR(50),
-	region							VARCHAR(50),
-	status							TINYINT			NOT NULL COMMENT 'UNRECEIVE:0, RECEIVED:1, USED:2, EXPIRED:3',
-	start_time		    	TIMESTAMP 	    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	limit								JSON 					COMMENT '{resource:.., sku:.., price:.., user:.., regoin:...}',
+	balance 						DECIMAL(8, 2) NOT NULL,
+	count 							INT 					NOT NULL,
+	limit_num 					INT 					NOT NULL DEFAULT 1,
+	start_time		    	TIMESTAMP 	  NOT NULL,
 	end_time       			TIMESTAMP   	NOT NULL,
 	create_time       	TIMESTAMP	    NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	mark 								TEXT,
 	PRIMARY KEY (id)
 );
+
+
+CREATE TABLE IF NOT EXISTS coupon_received
+(
+	coupon_id 					VARCHAR(50) 	NOT NULL,
+	user_id 						VARCHAR(50) 	NOT NULL,
+	num 								INT 					NOT NULL ,
+	left_num						INT 					NOT NULL ,
+	status							TINYINT				NOT NULL commnet '0: overtime, 1: received, 2: using, 3: used',
+	create_time       	TIMESTAMP	    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (coupon_id, user_id)
+);
+
+
+
 
 
 /**  Metering  **/
